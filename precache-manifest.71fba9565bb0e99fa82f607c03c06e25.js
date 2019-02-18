2cfd8dc62288ecfe8e4069da9b0f5f703ef763f3
CodeTestExample <2640199637@qq.com>
2019-02-18 21:04:25 +0800

    Add files via upload

---
+++ precache-manifest.71fba9565bb0e99fa82f607c03c06e25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "930ddc9bc83ed8d36cd4",
+    "url": "./static/css/main.e80742bb.chunk.css"
+  },
+  {
+    "revision": "930ddc9bc83ed8d36cd4",
+    "url": "./static/js/main.930ddc9b.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "634b417b1425393fb032",
+    "url": "./static/js/2.634b417b.chunk.js"
+  },
+  {
+    "revision": "029103c153bea4c20fd5e5be18b9cec8",
+    "url": "./index.html"
+  }
+];
